4423c2494d44436d1884dfc77e85c3aa821b7e55
Constance Mitchell <93297081+SnugglesMcGee@users.noreply.github.com>
2022-03-21 18:08:57 -0400

    hide pwd

--- models/User.js
+++ models/User.js
@@ -34,6 +34,21 @@ User.init(
     },
   },
   {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
+    },
     sequelize,
     timestamps: true,
     createdAt: true,
