336d4a704dccd2ad75a18ea2e73fbbbae997a7ca
Constance Mitchell <93297081+SnugglesMcGee@users.noreply.github.com>
2022-03-21 18:26:14 -0400

    bugs1

--- models/User.js
+++ models/User.js
@@ -4,33 +4,31 @@ const sequelize = require("../config/connection");
 class User extends Model {}
 
 User.init(
-  id,
   {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  username,
-  {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email,
-  {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    validate: {
-      isEmail: true,
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-  },
-  password,
-  {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      len: [8],
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
     },
   },
   {
