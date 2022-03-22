66383af34328faa7aea38053a041d062e8807f1c
Constance Mitchell <93297081+SnugglesMcGee@users.noreply.github.com>
2022-03-22 13:42:56 -0400

    approval

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require("./User");
 const Post = require("./Post");
+const Approval = require("./Approval");
 
 User.hasMany(Post, {
   foreignKey: "user_id",
@@ -9,4 +10,32 @@ Post.belongsTo(User, {
   foreignKey: "user_id",
 });
 
-module.exports = { User, Post };
+User.belongsToMany(Post, {
+  through: Approval,
+  as: "approved_posts",
+  foreignKey: "user_id",
+});
+
+Post.belongsToMany(User, {
+  through: Approval,
+  as: "approved_posts",
+  foreignKey: "post_id",
+});
+
+Approval.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Approval.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+Approval.hasMany(Vote, {
+  foreignKey: "user_id",
+});
+
+Approval.hasMany(Vote, {
+  foreignKey: "post_id",
+});
+
+module.exports = { User, Post, Approval };
