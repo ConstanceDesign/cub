1221599245af1d79ce01fb7bcc52d1a0804839d2
Constance Mitchell <93297081+SnugglesMcGee@users.noreply.github.com>
2022-03-22 09:44:22 -0400

    force true

--- models/index.js
+++ models/index.js
@@ -5,4 +5,12 @@ module.exports = { User };
 const User = require("./User");
 const Post = require("./Post");
 
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
 module.exports = { User, Post };
--- server.js
+++ server.js
@@ -8,16 +8,16 @@ const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3306;
 
-// app.engine("handlebars", hbs.engine);
-// app.set("view engine", "handlebars");
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 // turn on routes
 app.use(routes);
 
 // turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
