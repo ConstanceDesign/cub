131ba0ba3de0a26203bdd71773689b02b2a5d0f1
Constance Mitchell <93297081+SnugglesMcGee@users.noreply.github.com>
2022-03-22 10:52:22 -0400

    user-routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { User } = require("../../models");
+// const { User } = require("../../models");
 
 router.get("/:id", (req, res) => {
   User.findOne({
