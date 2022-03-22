807bd94f963ca950ea3cc492bc17c1ef11fdc93f
Constance Mitchell <93297081+SnugglesMcGee@users.noreply.github.com>
2022-03-22 16:34:53 -0400

    1st frontend

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const routes = require("./routes");
 const sequelize = require("./config/connection");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 // turn on routes
 app.use(routes);
