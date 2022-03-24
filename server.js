7cbc1e8786e89595548b082d7047baad530cfd85
Constance Mitchell <93297081+SnugglesMcGee@users.noreply.github.com>
2022-03-24 11:30:04 -0400

    tblshoot1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
-const sequelize = require("./config/connection");
+// const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
