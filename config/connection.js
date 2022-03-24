827bbf1256d415790e8952e6948e2413465f4d5a
Constance Mitchell <93297081+SnugglesMcGee@users.noreply.github.com>
2022-03-24 11:09:39 -0400

    host

--- config/connection.js
+++ config/connection.js
@@ -12,20 +12,11 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PW,
     {
-      host: "localhost",
+      host: "xlf3ljx3beaucz9x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
       dialect: "mysql",
       port: 3306,
     }
   );
 }
-
-// create connection to our db
-// const sequelize = process.env.JAWSDB_URL
-//   ? new Sequelize(process.env.JAWSDB_URL)
-//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-//       host: "localhost",
-//       dialect: "mysql",
-//       port: 3306,
-//     });
 
 module.exports = sequelize;
