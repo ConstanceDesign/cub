f67a506975aefb0191aa12c3ddd9e394c50e61ac
Constance Mitchell <93297081+SnugglesMcGee@users.noreply.github.com>
2022-03-24 11:14:47 -0400

    bug

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-const sequelize = require("./config/connection");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
   secret: "Super secret secret",
