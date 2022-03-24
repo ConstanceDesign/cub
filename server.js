39ab06a38494c60796aacc333bfc9b085c1706db
Constance Mitchell <93297081+SnugglesMcGee@users.noreply.github.com>
2022-03-24 11:05:39 -0400

    host

--- server.js
+++ server.js
@@ -29,11 +29,11 @@ app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(require("./controllers/"));
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now listening"));
+  app.listen(PORT, () => console.log(`Now listening ${PORT}!`));
 });
