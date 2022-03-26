619150e32c939a43abcbc8fa0fcff11802a2f355
Constance Mitchell <93297081+SnugglesMcGee@users.noreply.github.com>
2022-03-26 15:04:08 -0400

    logout

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -11,4 +11,64 @@ async function logout() {
   }
 }
 
-document.querySelector("#logout").addEventListener("click", logout);
+// document.querySelector("#logout").addEventListener("click", logout);
+
+document.querySelector(".logout").addEventListener("click", logout);
+
+// async function logoutFormHandler(event) {
+//   event.preventDefault();
+
+//   const email = document.querySelector("#email-login").value.trim();
+//   const password = document.querySelector("#password-login").value.trim();
+
+//   if (email && password) {
+//     const response = await fetch("/api/users/login", {
+//       method: "post",
+//       body: JSON.stringify({
+//         email,
+//         password,
+//       }),
+//       headers: { "Content-Type": "application/json" },
+//     });
+
+//     if (response.ok) {
+//       document.location.replace("/dashboard/");
+//     } else {
+//       alert(response.statusText);
+//     }
+//   }
+// }
+
+// async function signupFormHandler(event) {
+//   event.preventDefault();
+
+//   const username = document.querySelector("#username-signup").value.trim();
+//   const email = document.querySelector("#email-signup").value.trim();
+//   const password = document.querySelector("#password-signup").value.trim();
+
+//   if (username && email && password) {
+//     const response = await fetch("/api/users", {
+//       method: "post",
+//       body: JSON.stringify({
+//         username,
+//         email,
+//         password,
+//       }),
+//       headers: { "Content-Type": "application/json" },
+//     });
+
+//     if (response.ok) {
+//       document.location.replace("/dashboard/");
+//     } else {
+//       alert(response.statusText);
+//     }
+//   }
+// }
+
+// document
+//   .querySelector(".login-form")
+//   .addEventListener("submit", loginFormHandler);
+
+// document
+//   .querySelector(".signup-form")
+//   .addEventListener("submit", signupFormHandler);
