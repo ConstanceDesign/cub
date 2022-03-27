739e7399ac11563bd6052f46a5d34794e36971a3
Constance Mitchell <93297081+SnugglesMcGee@users.noreply.github.com>
2022-03-27 10:12:45 -0400

    frontend14

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 const userRoutes = require("./user-routes");
 const postRoutes = require("./post-routes");
 const commentRoutes = require("./comment-routes");
+const messageRoutes = require("./message-routes");
 
 router.use("/users", userRoutes);
 router.use("/posts", postRoutes);
