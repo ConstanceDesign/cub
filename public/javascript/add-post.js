9bec07e0a82065d9387436b8cb033e78fb0d9e87
Constance Mitchell <93297081+SnugglesMcGee@users.noreply.github.com>
2022-03-26 22:41:09 -0400

    frontend10

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -2,7 +2,7 @@ async function newFormHandler(event) {
   event.preventDefault();
 
   const title = document.querySelector('input[name="post-title"]').value;
-  const post_url = document.querySelector('input[name="category"]').value;
+  const post_url = document.querySelector('input[name="post-category"]').value;
 
   const response = await fetch(`/api/posts`, {
     method: "POST",
