d3758bf0007eac92bb902f16d2b02a3160e70280
Constance Mitchell <93297081+SnugglesMcGee@users.noreply.github.com>
2022-03-29 16:23:16 -0400

    dtw1

--- server.js
+++ server.js
@@ -51,6 +51,16 @@ app.use(require("./controllers/"));
 //   });
 // });
 
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
 document.querySelector("#copyright-year").innerText = new sequelize.sync({
   force: false,
 }).then(() => {
